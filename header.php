8a27fd9d0c877f50040a42d39b24fbe0b34a95a6
SamPM <63992604+SamPM092@users.noreply.github.com>
2020-12-14 13:54:45 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+          <title></title>
+          <link rel="stylesheet" href="css/a.css">
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+          <div class="wrapper">
+            <div class="header">
+              <div class="logo">
+                <img src="css/logo.png" width="200px">
+              </div>
+            </div>
+              <div class="panel">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="logout.php">Logout</a></li>
+        </ul>
+      </div>
+      <div class="content">
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'header.php';
+
+?>
+
+<h1> Selamat Datang (Nama Mahasiswa) </h1>
+<h5> Dengan NIM (NIM) </h4>
+<a href="chat_dosen.php"> (Ambil nama dosen dari database) </a>
+
+<div class="tugas">
+  <span class="list_tugas"> 1 </span>
+  <span class="list_tugas"> 2 </span>
+  <span class="list_tugas"> 3 </span>
+  <span class="list_tugas"> 4 </span>
+  <span class="list_tugas"> 5 </span>
+</div>
